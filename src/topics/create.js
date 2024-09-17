136571837f423f048477d20281c622f3075a30d5
AlAnoud2003 <amkhulai@andrew.cmu.edu>
2024-09-17 19:12:50 +0300

    Added my P1 changes into src/topics/create.js

--- src/topics/create.js
+++ src/topics/create.js
@@ -109,15 +109,15 @@ module.exports = function (Topics) {
 		// Check if the category exists
 		if (!categoryExists) {
 			throw new Error('[[error:no-category]]');
-        }
+		}
 
 		// Check if the user has the necessary privileges to create or tag
 		const cannotCreate = !canCreate;
 		const cannotTag = !canTag && data.tags.length;
-	
+
 		if (cannotCreate || cannotTag) {
-            throw new Error('[[error:no-privileges]]');
-        }
+			throw new Error('[[error:no-privileges]]');
+		}
 
 		console.log('Al Anoud: Refactored code executed');
 
