747709aeddc26f687210ab50030981b508179d41
Hakaabi <hakaabi@andrew.cmu.edu>
2024-09-25 16:28:10 +0300

    trying to fix issues in src/topics/posts.js

--- src/topics/posts.js
+++ src/topics/posts.js
@@ -16,7 +16,7 @@ const backlinkRegex = new RegExp(`(?:${nconf.get('url').replace('/', '\\/')}|\b|
 
 module.exports = function (Topics) {
 	Topics.onNewPostMade = async function (postData) {
-		console.log('this is where new post code is!!!');	  
+		console.log('this is where new post code is!!!');  
 		await Topics.updateLastPostTime(postData.tid, postData.timestamp);
 		await Topics.addPostToTopic(postData.tid, postData);
 	};
