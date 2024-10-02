d2df83b748027ae8dc8c9cb963f1070e69015843
Ruba Al Mahmoud <Rubaalmahmoud@dyn-172-20-102-226.qatar.cmu.local>
2024-10-02 13:45:03 +0300

    Fix unused variable error for resourcesButtonController in routes/index.js

--- src/controllers/resources-button.js
+++ src/controllers/resources-button.js
@@ -3,10 +3,10 @@
 const controllers = {};
 
 controllers.getResourcesButtonPage = async (req, res) => {
-    // Render the resources-button template
-    res.render('resources-button', {
-        title: 'Resources Page' // You can customize the title or add more data as needed
-    });
+	// Render the resources-button template
+	res.render('resources-button', {
+		title: 'Resources Page', // You can customize the title or add more data as needed
+	});
 };
 
 module.exports = controllers;
