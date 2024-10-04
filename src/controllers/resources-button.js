a72adf60e138ee2c0681ae104a757f669062ae2d
Ruba Al Mahmoud <Rubaalmahmoud@MacBook-Air-8.local>
2024-10-04 14:35:59 +0300

    fixing styling issue

--- src/controllers/resources-button.js
+++ src/controllers/resources-button.js
@@ -2,9 +2,9 @@
 
 const controllers = {};
 controllers.getResourcesButtonPage = async (req, res) => {
-    res.render('resources-button', {
-        title: 'Resources Page' // You can customize the title or add more data as needed
-    });
+	res.render('resources-button', {
+		title: 'Resources Page', // You can customize the title or add more data as needed
+	});
 };
 
 module.exports = controllers;
