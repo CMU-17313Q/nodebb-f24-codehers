ca07be59d4d10eac56f5a5335937f09f784fb1b4
Ruba Al Mahmoud <Rubaalmahmoud@MacBook-Air-8.local>
2024-10-04 14:32:46 +0300

    fixing-indentation-issues

--- src/controllers/resources-button.js
+++ src/controllers/resources-button.js
@@ -1,9 +1,7 @@
 'use strict';
 
 const controllers = {};
-
 controllers.getResourcesButtonPage = async (req, res) => {
-    // Render the resources-button template
     res.render('resources-button', {
         title: 'Resources Page' // You can customize the title or add more data as needed
     });
