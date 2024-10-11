bde6212387a6033eaa7bac9a86f609f83702c7f7
AlAnoud2003 <amkhulai@andrew.cmu.edu>
2024-10-11 14:52:20 +0300

    Combiend 2 definitions of getResourcesButtonPage into one definition

--- src/controllers/resources-button.js
+++ src/controllers/resources-button.js
@@ -1,24 +1,21 @@
 'use strict';
 
+const db = require('../database');
+
 const controllers = {};
 
 controllers.getResourcesButtonPage = async (req, res) => {
-	// Render the resources-button template
-	res.render('resources-button', {
-		title: 'Resources Page', // You can customize the title or add more data as needed
-	});
+    try {
+        const links = await db.getSetMembers('resources:links');
+        console.log('Retrieved links:', links); // Debugging
+        res.render('resources-button', {
+            title: 'Resources Page', // You can customize the title or add more data as needed
+            links,
+        });
+    } catch (err) {
+        console.error('Error retrieving links:', err);
+        res.status(500).send('Internal Server Error');
+    }
 };
 
 module.exports = controllers;
-
-
-const db = require('../database');
-
-async function getResourcesButtonPage(req, res) {
-	const links = await db.getSetMembers('resources:links');
-	res.render('resources-button', { links });
-}
-
-module.exports = {
-	getResourcesButtonPage,
-};
