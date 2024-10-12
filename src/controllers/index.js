91b525eb27cc0c538da0eaf4f1b997b1e0bd6a94
AlAnoud2003 <amkhulai@andrew.cmu.edu>
2024-10-12 16:19:59 +0300

    Reverted changes in controllers/inndex

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -37,7 +37,7 @@ Controllers.osd = require('./osd');
 Controllers['404'] = require('./404');
 Controllers.errors = require('./errors');
 Controllers.composer = require('./composer');
-Controllers.resources = require('./resources'); // Include the file extension
+Controllers.resourcesButton = require('./resources-button'); // Include the file extension
 
 
 Controllers.write = require('./write');
