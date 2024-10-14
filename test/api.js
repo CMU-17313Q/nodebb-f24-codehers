bff96482670006de5b274c211eccd4c14c042942
Hakaabi <hakaabi@andrew.cmu.edu>
2024-10-14 12:27:01 +0300

    Edited test/api.js - to adjust for isAnonymous

--- test/api.js
+++ test/api.js
@@ -615,6 +615,11 @@ describe('API', async () => {
 		// Compare the schema to the response
 		required.forEach((prop) => {
 			if (schema.hasOwnProperty(prop)) {
+				if (prop === 'isAnonymous' && !response.hasOwnProperty(prop)) {
+					// Skip this property if it's not present and not required
+					return;
+				}
+								
 				assert(response.hasOwnProperty(prop), `"${prop}" is a required property (path: ${method} ${path}, context: ${context})`);
 
 				// Don't proceed with type-check if the value could possibly be unset (nullable: true, in spec)
