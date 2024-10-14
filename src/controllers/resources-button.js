74924f5a83c03fa00c3bbaf790df868396cc3c06
AlAnoud2003 <amkhulai@andrew.cmu.edu>
2024-10-14 17:10:38 +0300

    Passed the links as a JSON object to the template

--- src/controllers/resources-button.js
+++ src/controllers/resources-button.js
@@ -6,15 +6,14 @@ const db = require('../database');
 const controllers = {};
 
 controllers.getResourcesButtonPage = async (req, res) => {
-	try {
-		const links = await db.getSetMembers('resources:links');
-		const cleanedLinks = links.map(link => link.replace(/\]\(.*?\)/g, ''));
-		const linksHtml = cleanedLinks.map(link => `<li><a href="${link}" target="_blank">${link}</a></li>`).join('');
-		console.log('Generated links HTML:', linksHtml); // Log the generated HTML to the console
-		res.render('resources-button', {
-			title: 'Resources Page', // You can customize the title or add more data as needed
-			linksHtml,
-		});
+    try {
+        const links = await db.getSetMembers('resources:links');
+        const cleanedLinks = links.map(link => link.replace(/\]\(.*?\)/g, ''));
+        console.log('Cleaned links:', cleanedLinks); // Log the cleaned links to the console
+        res.render('resources-button', {
+            title: 'Resources Page', // You can customize the title or add more data as needed
+            links: JSON.stringify(cleanedLinks), // Pass links as a JSON string
+        });
 	} catch (err) {
 		console.error('Error retrieving links:', err);
 		res.status(500).send('Internal Server Error');
