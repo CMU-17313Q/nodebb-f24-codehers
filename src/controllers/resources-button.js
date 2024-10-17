7a6af868539de8f9d1a18b9eee12d387c7ecc6a2
AlAnoud2003 <amkhulai@andrew.cmu.edu>
2024-10-18 02:46:42 +0300

    Updated code in resources-button.js to use the message property

--- src/controllers/resources-button.js
+++ src/controllers/resources-button.js
@@ -10,13 +10,29 @@ controllers.getResourcesButtonPage = async (req, res) => {
 		const links = await db.getSetMembers('resources:links');
 		const cleanedLinks = links.map(link => link.replace(/\]\(.*?\)/g, ''));
 		console.log('Extracted links:', cleanedLinks); // Log the cleaned links to the console
-		res.render('resources-button', {
-			title: 'Resources Page', // You can customize the title or add more data as needed
-			links: JSON.stringify(cleanedLinks),
-		});
+		
+     	// Construct the response object
+	  	const response = {
+			message: 'Links fetched successfully',
+			links: cleanedLinks,
+		};
+
+		// Send the response
+		res.status(200).json(response);
+
+
+		//res.render('resources-button', {
+			//title: 'Resources Page', // You can customize the title or add more data as needed
+			//links: JSON.stringify(cleanedLinks),
+		//});
 	} catch (err) {
 		console.error('Error retrieving links:', err);
-		res.status(500).send('Internal Server Error');
+		 // Send error response
+		 res.status(500).json({
+            message: 'An error occurred while fetching links',
+            error: err.message,
+        });
+		//res.status(500).send('Internal Server Error');
 	}
 };
 
