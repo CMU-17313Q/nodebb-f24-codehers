f9a6b77e989a0855e352bf56153a8c6b77f6802a
AlAnoud2003 <amkhulai@andrew.cmu.edu>
2024-10-18 16:34:52 +0300

    Syntax error

--- src/controllers/resources-button.js
+++ src/controllers/resources-button.js
@@ -44,19 +44,18 @@ controllers.getResourcesButtonPage = async (req, res) => {
 				errors: validate.errors,
 			});
 		}
-
-	    // Check the Accept header to determine the response type
+		// Check the Accept header to determine the response type
 		if (req.accepts('json')) {
 			// Send JSON response
 			return res.json(response);
-		} else {
-			// Render the HTML page
-			return res.render('resources-button', {
-				title: 'Resources Page', // You can customize the title or add more data as needed
-				message: response.message, // Include the message in the rendered HTML
-				links: cleanedLinks, // Pass the cleaned links directly
-			});
 		}
+		// Render the HTML page
+		return res.render('resources-button', {
+			title: 'Resources Page', // You can customize the title or add more data as needed
+			message: response.message, // Include the message in the rendered HTML
+			links: cleanedLinks, // Pass the cleaned links directly
+		});
+
 
 		// res.render('resources-button', {
 		// title: 'Resources Page', // You can customize the title or add more data as needed
